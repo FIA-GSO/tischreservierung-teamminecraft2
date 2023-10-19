eefa04541ceef08af165af95bfdb954540a7a511
Hakan Kök <hakan.k10@gso.schule.koeln>
2023-10-19 13:53:44 +0200

    Init /table endpoint

--- api/api.py
+++ api/api.py
@@ -1,12 +1,33 @@
 import flask
 from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
 
-@app.route('/', methods=['GET'])
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def eval_bool_arg(arg_value) -> None|bool:
+    if isinstance(arg_value, str) and arg_value.lower() in ['true', '1']:
+        return True
+    return False
+
+@app.route('/api/v1/table', methods=['GET'])
 def home():
-    return "<h1>Tischreservierung</h1>"
+    if request.method == 'GET':
+        query = 'SELECT * FROM tische t'
+        if eval_bool_arg(request.args.get('free-tables')) == True:
+            # extend query to only match free tables
+            pass
+
+        connection = sqlite3.connect('buchungssystem.sqlite')
+        connection.row_factory = dict_factory
+        cursor = connection.cursor()
+        return jsonify(cursor.execute(query).fetchall())
 
 app.run()
