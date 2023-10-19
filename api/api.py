7188ec0d79fe871ffb66b0679b55532e9d95aa2f
Hakan Kök <hakan.k10@gso.schule.koeln>
2023-10-19 14:09:54 +0200

    Use free-at instead of free-tables

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 import flask
 from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
+
+from datetime import datetime
 import sqlite3
 
 app = flask.Flask(__name__)
@@ -12,17 +14,24 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-def eval_bool_arg(arg_value) -> None|bool:
+def get_bool_arg(arg_value) -> None|bool:
     if isinstance(arg_value, str) and arg_value.lower() in ['true', '1']:
         return True
     return False
+
+def get_datetime_arg(arg_value) -> None|datetime:
+    try:
+        return datetime.fromisoformat(arg_value)
+    except (TypeError, ValueError):
+        return None
 
 @app.route('/api/v1/table', methods=['GET'])
 def home():
     if request.method == 'GET':
         query = 'SELECT * FROM tische t'
-        if eval_bool_arg(request.args.get('free-tables')) == True:
-            # extend query to only match free tables
+        if get_datetime_arg(request.args.get('free-at')) != None:
+            # only return tables that are free until at least 1 hour from the given datetime
+            # extend query to only match qualifying tables
             pass
 
         connection = sqlite3.connect('buchungssystem.sqlite')
